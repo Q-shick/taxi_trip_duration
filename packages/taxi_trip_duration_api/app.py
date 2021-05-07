34dfbc3795eb8942362dc11e08ec48af581d3733
Q-shick <qshickkim@gmail.com>
2021-05-07 19:20:57 -0400

    Modified paths

--- packages/taxi_trip_duration_api/app.py
+++ packages/taxi_trip_duration_api/app.py
@@ -9,7 +9,8 @@ import plotly
 import plotly.graph_objs as go
 import plotly.express as px
 
-from taxi_trip_duration_api import config, pipeline as pp
+# from taxi_trip_duration_api 
+import config, pipeline as pp
 
 #----------------------------------------------------------------------------------
 
